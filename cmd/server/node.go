22241e830941cbdbedc0383b319352a4717f825e
Mugdha Adhav <mugdha.adhav@gmail.com>
2023-12-26 02:07:56 +0530

    Fix implementations for NodePublishVolume and NodeUnpublishVolume
    commit f0af073960946d926f22d8869d413ff2b624a9ce
    Author: Mugdha Adhav <mugdha.adhav@gmail.com>
    Date:   Tue Dec 26 02:06:46 2023 +0530
    
        Fix NodePublishVolume and NodeUnpublishVolume implementations

--- cmd/server/node.go
+++ cmd/server/node.go
@@ -2,27 +2,19 @@ package server
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
-	"k8s.io/kubernetes/pkg/volume/util/volumepathhandler"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"k8s.io/utils/mount"
 )
 
 const (
-	baseVolumeDir string = "/tmp/quickstart/volumes"
+	baseVolumeDir string = "/data/volumes"
 )
-
-func init() {
-	if err := os.MkdirAll(baseVolumeDir, os.ModeSticky|os.ModePerm); err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-}
 
 func (d *driver) NodeGetCapabilities(context.Context, *csi.NodeGetCapabilitiesRequest) (*csi.NodeGetCapabilitiesResponse, error) {
 	return &csi.NodeGetCapabilitiesResponse{
@@ -33,120 +25,74 @@ func (d *driver) NodeGetCapabilities(context.Context, *csi.NodeGetCapabilitiesRe
 func (d *driver) NodePublishVolume(ctx context.Context, req *csi.NodePublishVolumeRequest) (*csi.NodePublishVolumeResponse, error) {
 	targetPath := req.GetTargetPath()
 	log.Printf("NodePublishVolume: request received for volume %s with target-path: %s\n", req.VolumeId, targetPath)
-	driverPath := fmt.Sprintf("%s/%s", baseVolumeDir, req.VolumeId)
+	hostPath := fmt.Sprintf("%s/%s", baseVolumeDir, req.VolumeId)
 
+	// Create directory on the host
+	if err := os.MkdirAll(hostPath, 0777); err != nil {
+		return nil, fmt.Errorf("error creating host path: %w", err)
+	}
+	log.Printf("NodePublishVolume: created host-path directory: %s \n", hostPath)
+
+	// Check if targetPath exists and is a mount point
 	mounter := mount.New("")
-
-	// Create a block file.
-	_, err := os.Stat(driverPath)
+	notMnt, err := mount.IsNotMountPoint(mounter, targetPath)
 	if err != nil {
 		if os.IsNotExist(err) {
-			cmd := exec.Command("fallocate", "-l", "100M", driverPath)
-			output, err := cmd.CombinedOutput()
-			if err != nil {
-				return nil, fmt.Errorf("failed to create block device: %v, %v", err, string(output))
+			if err = os.Mkdir(targetPath, 0750); err != nil {
+				return nil, fmt.Errorf("error creating target path: %w", err)
 			}
+			notMnt = true
 		} else {
-			return nil, fmt.Errorf("failed to stat block device: %v, %v", driverPath, err)
+			return nil, fmt.Errorf("error while checking if target path is a mountpoint: %w", err)
 		}
 	}
 
-	// Associate block file with the loop device.
-	volPathHandler := volumepathhandler.VolumePathHandler{}
-	_, err = volPathHandler.AttachFileDevice(driverPath)
-	if err != nil {
-		// Remove the block file because it'll no longer be used again.
-		if err2 := os.Remove(driverPath); err2 != nil {
-			fmt.Printf("failed to cleanup block file %s: %v\n", driverPath, err2)
-		}
-		return nil, fmt.Errorf("failed to attach device %v: %v", driverPath, err)
+	if !notMnt {
+		return nil, status.Error(codes.Aborted, "target-path is not a mountpoint")
 	}
 
-	// Get loop device from the volume path.
-	loopDevice, err := volPathHandler.GetLoopDevice(driverPath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get the loop device: %w", err)
-	}
-
-	log.Println("NodePublishVolume: loop device", loopDevice)
-
-	// Check if the target path exists. Create if not present.
-	_, err = os.Lstat(targetPath)
-	if os.IsNotExist(err) {
-		if err = makeFile(targetPath); err != nil {
-			return nil, fmt.Errorf("failed to create target path: %w", err)
-		}
-	}
-	if err != nil {
-		return nil, fmt.Errorf("failed to check if the target block file exists: %w", err)
-	}
-
-	// Check if the target path is already mounted, if yes prevent remounting.
-	notMountPoint, err := mount.IsNotMountPoint(mounter, targetPath)
-	if err != nil {
-		if !os.IsNotExist(err) {
-			return nil, fmt.Errorf("error checking path %s for mount: %w", targetPath, err)
-		}
-		// Target Path is ready for mounting.
-		notMountPoint = true
-	}
-	if !notMountPoint {
-		// Target path is already mounted, return gracefully.
-		return &csi.NodePublishVolumeResponse{}, nil
-	}
-
-	// Mounting the volume.
+	// Mount hostPath on the targetPath
 	options := []string{"bind"}
-	if err := mounter.Mount(loopDevice, targetPath, "", options); err != nil {
-		return nil, fmt.Errorf("failed to mount block device: %s at %s: %w", driverPath, targetPath, err)
+	if err := mounter.Mount(hostPath, targetPath, "", options); err != nil {
+		return nil, fmt.Errorf("failed to mount device: %s at %s", hostPath, targetPath)
 	}
-	log.Printf("NodePublishVolume: volume succesesfully mounted at: %s for volume: %s", targetPath, req.VolumeId)
+	log.Printf("NodePublishVolume: host path %s successfully mounted at %s for volume: %s", hostPath, targetPath, req.VolumeId)
 
 	return &csi.NodePublishVolumeResponse{}, nil
 }
 
 func (d *driver) NodeUnpublishVolume(ctx context.Context, req *csi.NodeUnpublishVolumeRequest) (*csi.NodeUnpublishVolumeResponse, error) {
 	targetPath := req.GetTargetPath()
-	driverPath := fmt.Sprintf("%s/%s", baseVolumeDir, req.VolumeId)
+	hostPath := fmt.Sprintf("%s/%s", baseVolumeDir, req.VolumeId)
+	log.Printf("NodeUnpublishVolume: request received for volume %s with target-path %s\n", req.VolumeId, targetPath)
 
-	fileExists := func(path string) {
-		_, err := os.Stat(path)
-
-		if errors.Is(err, os.ErrNotExist) {
-			log.Println("NodeUnPublishVolume: Path doesn't exist: ", path)
-			return
-		}
-		log.Println("NodeUnPublishVolume: Path exists: ", path)
-	}
-
-	fileExists(targetPath)
-	fileExists(driverPath)
-
-	// Unmount only if the target path is really a mount point.
+	// Unmount only if the target path exists and is really a mount point.
 	if notMountPoint, err := mount.IsNotMountPoint(mount.New(""), targetPath); err != nil {
 		if !os.IsNotExist(err) {
-			return nil, fmt.Errorf("check target path: %w", err)
+			return nil, status.Error(codes.NotFound, "couldn't find target-path")
 		}
 	} else if !notMountPoint {
-		// Unmounting the image or filesystem.
+		// Unmounting the volume/directory.
 		err = mount.New("").Unmount(targetPath)
 		if err != nil {
 			return nil, fmt.Errorf("unmount target path: %w", err)
 		}
+
+		log.Println("NodeUnpublishVolume: unmount successful for target path", targetPath)
 	}
+
 	// Delete the mount point.
 	if err := os.RemoveAll(targetPath); err != nil {
 		return nil, fmt.Errorf("remove target path: %w", err)
 	}
+	log.Println("NodeUnpublishVolume: attempted deletion of target path", targetPath)
 
-	// Delete the volume
-	if err := os.RemoveAll(driverPath); err != nil {
+	// Delete the hostpath.
+	if err := os.RemoveAll(hostPath); err != nil {
 		return nil, fmt.Errorf("remove host path: %w", err)
 	}
-	log.Printf("NodeUnPublishVolume: host path deleted %s with target-path: %s\n", req.VolumeId, targetPath)
+	log.Println("NodeUnpublishVolume: attempted deletion of host path", hostPath)
 
-	fileExists(targetPath)
-	fileExists(driverPath)
 	return &csi.NodeUnpublishVolumeResponse{}, nil
 }
 
@@ -170,18 +116,4 @@ func (d *driver) NodeGetInfo(context.Context, *csi.NodeGetInfoRequest) (*csi.Nod
 	return &csi.NodeGetInfoResponse{
 		NodeId: d.config.nodeID,
 	}, nil
-}
-
-// makeFile ensures that the file exists, creating it if necessary.
-// The parent directory must exist.
-func makeFile(pathname string) error {
-	f, err := os.OpenFile(pathname, os.O_CREATE, os.FileMode(0o644))
-	if err != nil {
-		if !os.IsExist(err) {
-			return err
-		}
-	}
-	defer f.Close()
-
-	return nil
 }
--- docs/configure-and-deploy.md
+++ docs/configure-and-deploy.md
@@ -22,14 +22,38 @@
     * Create a socket at the following path `/var/lib/kubelet/plugins/[CSIDriverName]/csi.sock` on the host and mount it in your pod at `/csi`.
     * This needs to be mounted on all containers.
 
-2. Plugins registry
+1. Plugins registry
     * Mount `/var/lib/kubelet/plugins_registry` from the host at `/registration`.
     * Only mount in `node-driver-registrar` container.
 
-3. Kubelet directory
-    * Mount `/var/lib/kubelet` from the host at the same path on the pod.
+1. Kubelet pods directory
+    * Mount `/var/lib/kubelet/pods` from the host at the same path on the pod.
+    * Only mount in your plugin container.
+    * [REQUIRED] [Bi-directional](https://kubernetes.io/docs/concepts/storage/volumes/#mount-propagation) mount propagation.
+
+1. Kubelet plugins directory
+    * Mount `/var/lib/kubelet/plugins` from the host at the same path on the pod.
     * Only mount in your plugin container.
     * [REQUIRED] [Bi-directional](https://kubernetes.io/docs/concepts/storage/volumes/#mount-propagation) mount propagation.
 
 
 More details in [k8s docs](https://github.com/kubernetes/design-proposals-archive/blob/main/storage/container-storage-interface.md#kubelet-to-csi-driver-communication).
+
+### Issues faced
+#### Mounting kubelet directory instead of child pod and plugins directory
+In the design proposals archive doc for CSI, it's mentioned to mount only the kubelet directory, instead of pods and plugins kubelet directories separately.
+
+**What went wrong**
+I tried mounting `/var/lib/kubelet` directory from the host onto the plugins container, but it didn't mount the host-path onto the target-path.
+
+**Debugging steps**
+While trying to create the target-directory in `NodePublishVolume` function, it was failing with error path doesn't exist.
+
+To workaround this, I tried creating the target-directory with `MkdirAll()` function, which would create the entire path even if it didn't exist, but it still didn't mount the expected target-directory.
+
+Also checked the mount paths on the host using `mount` command and listing contents in `/proc/mounts` directory but this didn't help.
+
+Finally while getting list of mounted filesystems from `/etc/mtab` which stores the mount and umount commands that were executed, I found that the mount was hapenning on `/var/lib/kubelet/<pod-id>` instead of `/var/lib/kubelet/pod/<pod-id>`.
+
+**Fix:**
+After mounting the `/var/lib/kubelet/pods` and `/var/lib/kubelet/plugins` directories separately on plugin container, the mounting of host-path on target-path worked.
